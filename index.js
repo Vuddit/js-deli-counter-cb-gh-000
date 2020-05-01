41188364c0a3da4f7c1b2d47af8ac27b027c2a91
Rohit Kale <ruk22@cam.ac.uk>
2020-05-01 23:47:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber (line, name) {
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine[0]}.`
-    katzDeliLine.shift()
+    katzDeliLine.shift(1)
   } else {
     return "There is nobody waiting to be served!"
   }
