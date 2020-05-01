e983bc627b2f7ea456c2e8fb5511c5d138727af2
Rohit Kale <ruk22@cam.ac.uk>
2020-05-01 23:44:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ function takeANumber (line, name) {
 
 function nowServing (katzDeliLine) {
   return katzDeliLine[0]
-  katzDeliLine.unshift()
+  if (katzDeliLine.length > 0) {
+    return katzDeliLine[0]
+    katzDeliLine.unshift()
+  }
+  else
 }
